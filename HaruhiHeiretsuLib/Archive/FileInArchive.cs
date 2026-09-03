882d58806c38fcd9a7b1ea96856a11506395e08c
jonko0493 <jonko0493@protonmail.com>
2026-09-03 06:27:02 -0700

    More file scoped namespaces

--- HaruhiHeiretsuLib/Archive/FileInArchive.cs
+++ HaruhiHeiretsuLib/Archive/FileInArchive.cs
@@ -2,139 +2,138 @@
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace HaruhiHeiretsuLib.Archive
+namespace HaruhiHeiretsuLib.Archive;
+
+/// <summary>
+/// A representation of a file contained within an archive
+/// </summary>
+public class FileInArchive
 {
     /// <summary>
-    /// A representation of a file contained within an archive
+    /// The ID of the file in the MCB (if it is in the MCB)
     /// </summary>
-    public class FileInArchive
+    [JsonIgnore]
+    public ushort McbId { get; set; }
+    /// <summary>
+    /// The location of the file in the MCB (if it is in the MCB)
+    /// </summary>
+    [JsonIgnore]
+    public (int parent, int child) Location { get; set; } = (-1, -1);
+    /// <summary>
+    /// The data stored in the MCB that determines where this file is located in a bin archive (if it is in the MCB)
+    /// </summary>
+    [JsonIgnore]
+    public (int ArchiveIndex, int ArchiveOffset) McbEntryData { get; set; }
+    [JsonIgnore]
+    internal uint MagicInteger { get; set; }
+    /// <summary>
+    /// The file's index in a bin archive (if in a bin archive)
+    /// </summary>
+    public int BinArchiveIndex { get; set; }
+    /// <summary>
+    /// The offset of the file in a bin archive (if in a bin archive)
+    /// </summary>
+    [JsonIgnore]
+    public int Offset { get; set; }
+    /// <summary>
+    /// The length of the file
+    /// </summary>
+    [JsonIgnore]
+    public int Length { get; internal set; }
+    /// <summary>
+    /// The binary data representation of teh file
+    /// </summary>
+    [JsonIgnore]
+    public List<byte> Data { get; set; }
+    /// <summary>
+    /// The compressed data of the file in the archive
+    /// </summary>
+    [JsonIgnore]
+    public byte[] CompressedData { get; internal set; }
+    /// <summary>
+    /// A boolean representing whether the file has been edited or not
+    /// </summary>
+    [JsonIgnore]
+    public bool Edited { get; set; }
+
+    /// <summary>
+    /// Initializes this file with its binary data
+    /// </summary>
+    /// <param name="decompressedData">The decompressed binary data of the file</param>
+    /// <param name="offset">The offset of the file in the bin archive</param>
+    public virtual void Initialize(byte[] decompressedData, int offset)
     {
-        /// <summary>
-        /// The ID of the file in the MCB (if it is in the MCB)
-        /// </summary>
-        [JsonIgnore]
-        public ushort McbId { get; set; }
-        /// <summary>
-        /// The location of the file in the MCB (if it is in the MCB)
-        /// </summary>
-        [JsonIgnore]
-        public (int parent, int child) Location { get; set; } = (-1, -1);
-        /// <summary>
-        /// The data stored in the MCB that determines where this file is located in a bin archive (if it is in the MCB)
-        /// </summary>
-        [JsonIgnore]
-        public (int ArchiveIndex, int ArchiveOffset) McbEntryData { get; set; }
-        [JsonIgnore]
-        internal uint MagicInteger { get; set; }
-        /// <summary>
-        /// The file's index in a bin archive (if in a bin archive)
-        /// </summary>
-        public int BinArchiveIndex { get; set; }
-        /// <summary>
-        /// The offset of the file in a bin archive (if in a bin archive)
-        /// </summary>
-        [JsonIgnore]
-        public int Offset { get; set; }
-        /// <summary>
-        /// The length of the file
-        /// </summary>
-        [JsonIgnore]
-        public int Length { get; internal set; }
-        /// <summary>
-        /// The binary data representation of teh file
-        /// </summary>
-        [JsonIgnore]
-        public List<byte> Data { get; set; }
-        /// <summary>
-        /// The compressed data of the file in the archive
-        /// </summary>
-        [JsonIgnore]
-        public byte[] CompressedData { get; internal set; }
-        /// <summary>
-        /// A boolean representing whether the file has been edited or not
-        /// </summary>
-        [JsonIgnore]
-        public bool Edited { get; set; }
-
-        /// <summary>
-        /// Initializes this file with its binary data
-        /// </summary>
-        /// <param name="decompressedData">The decompressed binary data of the file</param>
-        /// <param name="offset">The offset of the file in the bin archive</param>
-        public virtual void Initialize(byte[] decompressedData, int offset)
-        {
-            Data = [.. decompressedData];
-            Offset = offset;
-        }
-        /// <summary>
-        /// Gets the binary data representation of the file
-        /// </summary>
-        /// <returns>A byte array containing the binary representation of the file</returns>
-        public virtual byte[] GetBytes()
-        {
-            return [.. Data];
-        }
-
-        /// <summary>
-        /// Empty constructor
-        /// </summary>
-        public FileInArchive()
-        {
-        }
-
-        /// <summary>
-        /// Casts this file to a specific subtype of file
-        /// </summary>
-        /// <typeparam name="T">The subtype of file to cast this file to</typeparam>
-        /// <returns>An object representing this file of the specified subtype</returns>
-        public T CastTo<T>() where T : FileInArchive, new()
-        {
-            T newFile = new()
-            {
-                McbId = McbId,
-                Location = Location,
-                McbEntryData = McbEntryData,
-                MagicInteger = MagicInteger,
-                BinArchiveIndex = BinArchiveIndex,
-                Offset = Offset,
-                Length = Length,
-                Data = Data,
-                CompressedData = CompressedData,
-                Edited = Edited
-            };
-            newFile.Initialize([.. Data], Offset);
-
-            return newFile;
-        }
+        Data = [.. decompressedData];
+        Offset = offset;
+    }
+    /// <summary>
+    /// Gets the binary data representation of the file
+    /// </summary>
+    /// <returns>A byte array containing the binary representation of the file</returns>
+    public virtual byte[] GetBytes()
+    {
+        return [.. Data];
     }
 
     /// <summary>
-    /// A static class for creating files
+    /// Empty constructor
     /// </summary>
-    /// <typeparam name="T">The type of file to create (must inherit FileInArchive)</typeparam>
-    public static class FileManager<T>
-        where T : FileInArchive, new()
+    public FileInArchive()
     {
-        /// <summary>
-        /// Creates a file from compressed data
-        /// </summary>
-        /// <param name="compressedData">The file's compressed data</param>
-        /// <param name="offset">(Optional) The offset of the file in the archive it comes from</param>
-        /// <param name="magicInteger">(Optional) The magic integer of the file</param>
-        /// <param name="index">(Optional) The index of the file in the archive</param>
-        /// <param name="length">(Optional) The length of the file</param>
-        /// <returns>A file object of type T</returns>
-        public static T FromCompressedData(byte[] compressedData, int offset = 0, uint magicInteger = 0, int index = -1, int length = -1)
+    }
+
+    /// <summary>
+    /// Casts this file to a specific subtype of file
+    /// </summary>
+    /// <typeparam name="T">The subtype of file to cast this file to</typeparam>
+    /// <returns>An object representing this file of the specified subtype</returns>
+    public T CastTo<T>() where T : FileInArchive, new()
+    {
+        T newFile = new()
         {
-            T created = new()
-            {
-                MagicInteger = magicInteger,
-                BinArchiveIndex = index,
-                Length = length,
-                Location = (-1, -1)
-            };
-            created.Initialize(Helpers.DecompressData(compressedData), offset);
-            return created;
-        }
+            McbId = McbId,
+            Location = Location,
+            McbEntryData = McbEntryData,
+            MagicInteger = MagicInteger,
+            BinArchiveIndex = BinArchiveIndex,
+            Offset = Offset,
+            Length = Length,
+            Data = Data,
+            CompressedData = CompressedData,
+            Edited = Edited
+        };
+        newFile.Initialize([.. Data], Offset);
+
+        return newFile;
+    }
+}
+
+/// <summary>
+/// A static class for creating files
+/// </summary>
+/// <typeparam name="T">The type of file to create (must inherit FileInArchive)</typeparam>
+public static class FileManager<T>
+    where T : FileInArchive, new()
+{
+    /// <summary>
+    /// Creates a file from compressed data
+    /// </summary>
+    /// <param name="compressedData">The file's compressed data</param>
+    /// <param name="offset">(Optional) The offset of the file in the archive it comes from</param>
+    /// <param name="magicInteger">(Optional) The magic integer of the file</param>
+    /// <param name="index">(Optional) The index of the file in the archive</param>
+    /// <param name="length">(Optional) The length of the file</param>
+    /// <returns>A file object of type T</returns>
+    public static T FromCompressedData(byte[] compressedData, int offset = 0, uint magicInteger = 0, int index = -1, int length = -1)
+    {
+        T created = new()
+        {
+            MagicInteger = magicInteger,
+            BinArchiveIndex = index,
+            Length = length,
+            Location = (-1, -1)
+        };
+        created.Initialize(Helpers.DecompressData(compressedData), offset);
+        return created;
     }
 }
--- HaruhiHeiretsuLib/Archive/McbSubArchive.cs
+++ HaruhiHeiretsuLib/Archive/McbSubArchive.cs
@@ -3,72 +3,71 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace HaruhiHeiretsuLib.Archive
+namespace HaruhiHeiretsuLib.Archive;
+
+public class McbSubArchive
 {
-    public class McbSubArchive
+    public ushort Id { get; set; }
+    public short Padding { get; set; }
+    public int Offset { get; set; }
+    public int Size { get; set; }
+
+    public List<FileInArchive> Files { get; set; } = [];
+
+    public McbSubArchive(int parentLoc, ushort id, short padding, int offset, int size, byte[] data)
     {
-        public ushort Id { get; set; }
-        public short Padding { get; set; }
-        public int Offset { get; set; }
-        public int Size { get; set; }
+        Id = id;
+        Padding = padding;
+        Offset = offset;
+        Size = size;
+        int childLoc = 0;
 
-        public List<FileInArchive> Files { get; set; } = [];
-
-        public McbSubArchive(int parentLoc, ushort id, short padding, int offset, int size, byte[] data)
+        for (int i = Offset; i < Offset + Size;)
         {
-            Id = id;
-            Padding = padding;
-            Offset = offset;
-            Size = size;
-            int childLoc = 0;
+            int archiveIndex = IO.ReadIntLE(data, i);
+            int archiveOffset = IO.ReadIntLE(data, i + 4);
+            int compressedSize = IO.ReadIntLE(data, i + 8);
 
-            for (int i = Offset; i < Offset + Size;)
+            if (archiveIndex == 0x7FFF)
             {
-                int archiveIndex = IO.ReadIntLE(data, i);
-                int archiveOffset = IO.ReadIntLE(data, i + 4);
-                int compressedSize = IO.ReadIntLE(data, i + 8);
-
-                if (archiveIndex == 0x7FFF)
-                {
-                    break;
-                }
-
-                byte[] compressedData = data.Skip(i + 12).Take(compressedSize).ToArray();
-
-                Files.Add(new() { Location = (parentLoc, childLoc++), Offset = i, McbId = Id, McbEntryData = (archiveIndex, archiveOffset), CompressedData = compressedData, Data = [.. Helpers.DecompressData(compressedData)] });
-
-                i += compressedSize + 12;
+                break;
             }
+
+            byte[] compressedData = data.Skip(i + 12).Take(compressedSize).ToArray();
+
+            Files.Add(new() { Location = (parentLoc, childLoc++), Offset = i, McbId = Id, McbEntryData = (archiveIndex, archiveOffset), CompressedData = compressedData, Data = [.. Helpers.DecompressData(compressedData)] });
+
+            i += compressedSize + 12;
         }
+    }
 
-        public byte[] GetBytes()
+    public byte[] GetBytes()
+    {
+        List<byte> bytes = [];
+
+        foreach (FileInArchive file in Files)
         {
-            List<byte> bytes = [];
+            bytes.AddRange(BitConverter.GetBytes(file.McbEntryData.ArchiveIndex));
+            bytes.AddRange(BitConverter.GetBytes(file.McbEntryData.ArchiveOffset));
 
-            foreach (FileInArchive file in Files)
+            if (!file.Edited)
             {
-                bytes.AddRange(BitConverter.GetBytes(file.McbEntryData.ArchiveIndex));
-                bytes.AddRange(BitConverter.GetBytes(file.McbEntryData.ArchiveOffset));
-
-                if (!file.Edited)
-                {
-                    bytes.AddRange(BitConverter.GetBytes(file.CompressedData.Length));
-                    bytes.AddRange(file.CompressedData);
-                }
-                else
-                {
-                    byte[] compressedData = Helpers.CompressData(file.GetBytes());
-                    int padding = (compressedData.Length % 0x800) == 0 ? 0x800 : 0x800 - (compressedData.Length % 0x800);
+                bytes.AddRange(BitConverter.GetBytes(file.CompressedData.Length));
+                bytes.AddRange(file.CompressedData);
+            }
+            else
+            {
+                byte[] compressedData = Helpers.CompressData(file.GetBytes());
+                int padding = (compressedData.Length % 0x800) == 0 ? 0x800 : 0x800 - (compressedData.Length % 0x800);
                     
-                    bytes.AddRange(BitConverter.GetBytes(compressedData.Length + padding));
-                    bytes.AddRange(compressedData);
-                    bytes.AddRange(new byte[padding]);
-                }
+                bytes.AddRange(BitConverter.GetBytes(compressedData.Length + padding));
+                bytes.AddRange(compressedData);
+                bytes.AddRange(new byte[padding]);
             }
-            bytes.AddRange(BitConverter.GetBytes(0x7FFF)); // end bytes
-            bytes.AddRange(new byte[bytes.Count % 0x1000 == 0 ? 0x1000 : 0x1000 - (bytes.Count % 0x1000)]);
-
-            return [.. bytes];
         }
+        bytes.AddRange(BitConverter.GetBytes(0x7FFF)); // end bytes
+        bytes.AddRange(new byte[bytes.Count % 0x1000 == 0 ? 0x1000 : 0x1000 - (bytes.Count % 0x1000)]);
+
+        return [.. bytes];
     }
 }
